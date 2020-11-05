6c933eb322d15fb522286710497462af9bf315ef
Matias Pizarro <pinkystylee98@gmail.com>
2020-11-05 16:13:20 -0300

    Parte 1
    casi listo, funciona en "." y el "|", el "*" le falta

---
+++ Tarea1P1/src/Tarea1P1/Transicion.java
@@ -0,0 +1,40 @@
+package Tarea1P1;
+
+public class Transicion {
+    
+    private String inicio;
+    private String union;
+    private String termino;
+
+    public Transicion(String inicio, String union, String termino){
+        this.inicio = inicio;
+        this.union = union;
+        this.termino = termino;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getUnion() {
+        return union;
+    }
+
+    public void setUnion(String union) {
+        this.union = union;
+    }
+
+    public String getTermino() {
+        return termino;
+    }
+
+    public void setTermino(String termino) {
+        this.termino = termino;
+    }
+
+    
+}
